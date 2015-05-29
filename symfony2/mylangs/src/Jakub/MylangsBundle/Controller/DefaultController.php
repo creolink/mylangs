5b1d27cd8675a3b3309fbdf946686489c57a37a7
Jakub Luczynski <jakub@gandalf.com.pl>
2015-05-29 11:22:59 +0200

    configuring

--- symfony2/mylangs/src/Jakub/MylangsBundle/Controller/DefaultController.php
+++ symfony2/mylangs/src/Jakub/MylangsBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name = 'aa')
     {
         return $this->render('JakubMylangsBundle:Default:index.html.twig', array('name' => $name));
     }
